7ada114ac954096098e2956a16a9a9db79a31c60
Seungyeon Oh <seungyeon_oh@hotmail.com>
2024-07-13 22:14:57 +0900

    Update 1_2classHW.dart

--- summerStudy/1_2classHW.dart
+++ summerStudy/1_2classHW.dart
@@ -6,17 +6,20 @@ class Account {
   Account(this.AccountNum, this.name, this._balance);
 
   void deposit(double amount) {
+    print("$amount원을 입금합니다.");
     _balance += amount;
   }
 
   void withdraw(double amount) {
     if (_balance < amount)
-      print('잔액이 부족해 출금할 수 없습니다.');
-    else
+      print('$amount원 인출을 시도하였으나 잔액이 부족해 출금할 수 없습니다.');
+    else {
+      print("$amount원을 인출합니다.");
       _balance -= amount;
+    }
   }
 
-  double get balance {
+  double getBalance() {
     return _balance;
   }
 }
@@ -29,9 +32,9 @@ class MaxLimitAccount extends Account {
 
   @override
   void deposit(double amount) {
-    var totalBalance = balance + amount;
+    var totalBalance = _balance + amount;
     if (totalBalance > maxLimit)
-      print('저축 가능한 최대 한도 $maxLimit을 초과하였습니다.');
+      print('$amount원 입금을 시도하였으나 저축 가능한 최대 한도 $maxLimit을 초과하였습니다.');
     else
       super.deposit(amount);
   }
@@ -40,19 +43,19 @@ class MaxLimitAccount extends Account {
 void main() {
   print('Account 클래스 테스트');
   Account account1 = Account(123456789, '홍길동', 500);
-  print('잔액: ${account1.balance}');
+  print('잔액: ${account1.getBalance()}');
   account1.deposit(1000);
-  print('잔액: ${account1.balance}'); //1500
+  print('잔액: ${account1.getBalance()}'); //1500
   account1.withdraw(700);
-  print('잔액: ${account1.balance}'); //800
+  print('잔액: ${account1.getBalance()}'); //800
   account1.withdraw(3000);
-  print('잔액: ${account1.balance}'); //800
+  print('잔액: ${account1.getBalance()}'); //800
 
   print('\nMaxLimitAccount 클래스 테스트');
   MaxLimitAccount account2 = MaxLimitAccount(11111111, '홍길동', 500, 2000);
-  print('잔액: ${account2.balance}'); //500
+  print('잔액: ${account2.getBalance()}'); //500
   account2.deposit(500);
-  print('잔액: ${account2.balance}'); //1000
+  print('잔액: ${account2.getBalance()}'); //1000
   account2.deposit(2000);
-  print('잔액: ${account2.balance}'); //1000
+  print('잔액: ${account2.getBalance()}'); //1000
 }
